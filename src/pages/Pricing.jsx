a8ab6dd3c15998f81799aa1bb9b3d73135855d17
ZoDD-Script <blessedchukwu89@gmail.com>
2024-12-04 12:49:27 +0100

    feat: routing with react-router-dom

---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,15 @@
+import PageNav from "../components/PageNav"
+
+function Pricing() {
+  return (
+    <div>
+      <PageNav />
+
+      <h1>
+        Pricing
+      </h1>
+    </div>
+  )
+}
+
+export default Pricing
