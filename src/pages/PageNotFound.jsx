0ca413bc3b77d4f3e64d3fbe414cb3edcfd923dd
ZoDD-Script <blessedchukwu89@gmail.com>
2024-12-04 15:06:50 +0100

    feat: Building the different pages, app layout and using nested routed and index route

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      Page Not found :(
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
-
-export default PageNotFound
