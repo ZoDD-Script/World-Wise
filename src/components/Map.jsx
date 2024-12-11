775b4737a3c52602172859ceb19627eed8958885
ZoDD-Script <blessedchukwu89@gmail.com>
2024-12-11 15:27:36 +0100

    updated: setting map position with geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,21 +6,31 @@ import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvent } from 'rea
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
 import flagemojiToPNG from '../../data/countryFlag';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
 
 function Map() {
-  const [mapPosition, setMapPosition] = useState([40, 0])
-
-  const [searchParams] = useSearchParams();
   const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0])
+  const [searchParams] = useSearchParams();
+  const {isLoading: isLoadingPosition, position: geolocationPosition, getPosition} = useGeolocation();
+
   const mapLat = searchParams.get('lat')
   const mapLng = searchParams.get('lng')
 
   useEffect(function() {
     if(mapLat && mapLng) setMapPosition([mapLat, mapLng])
-  }, [mapLat, mapLng])
+  }, [mapLat, mapLng]);
+
+  useEffect(function() {
+    if(geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+  }, [geolocationPosition])
 
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && <Button type='position'onClick={getPosition}>
+        {isLoadingPosition ? 'Loading...' : 'Use your position.'}
+      </Button>}
       <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
