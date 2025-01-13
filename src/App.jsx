ddf3d322d43646d40d22e382a089905661f8d77b
ZoDD-Script <blessedchukwu89@gmail.com>
2025-01-13 10:22:29 +0100

    update: Adding fake authentication, protecting a route

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import City from "./components/City"
 import Form from "./components/Form"
 import { CitiesProvider } from "./contexts/CitiesContext"
 import { AuthProvider } from "./contexts/FakeAuthContext"
+import ProtectedRoute from "./pages/ProtectedRoute"
 
 function App() {
   return (
@@ -23,12 +24,17 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
-                <Route index element={<Navigate replace to='cities' />} />
-                <Route path="cities" element={<CityList />} />
-                <Route path="cities/:id" element={<City />} />
-                <Route path="countries" element={<CountryList />} />
-                <Route path="form" element={<Form />} />
+            <Route path="app" element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to='cities' />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
             </Route>
             <Route path="*" element={<PageNotFound />} />
           </Routes>
